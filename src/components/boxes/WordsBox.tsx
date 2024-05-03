bb61bb4a6b5496cc58755a7ab0ae3dc70b441ff0
Joel P <wwwebjoyal@gmail.com>
2024-05-03 15:54:41 +0545

    Update

--- src/components/boxes/WordsBox.tsx
+++ src/components/boxes/WordsBox.tsx
@@ -1,21 +1,51 @@
-import React from 'react'
-import {Input1} from "@/components/common/Inputs";
-import {Button3} from "@/components/common/Buttons";
+'use client'
+import React from 'react';
 import Box from "@/components/common/Box";
+import { useSelector } from "react-redux";
+import { transformNumber } from "../../../utilities/transformNumber";
 
-const WordsBox = ()=>{
-    return(
-        <>
-            <Box>
-
-                    <div className={'font-black text-lg mb-5'}>words</div>
-
-
-
-
-            </Box>
-        </>
-    )
+interface Trapezoid {
+    selected: boolean;
+    data: string | null;
 }
 
-export default WordsBox
+interface Level {
+    octagon: (state: State) => State;
+    level: number;
+    selected: boolean;
+    trapezoid: Record<number, Trapezoid>;
+}
+
+interface State {
+    [levelKey: string]: Level;
+}
+
+const getNonNullTrapezoidDataValues = (state: State): string[] => {
+    const nonNullDataValues: string[] = [];
+
+    Object.values(state).forEach(level => {
+        Object.values(level.trapezoid).forEach(trapezoid => {
+            if (trapezoid.data !== null) {
+                nonNullDataValues.push(trapezoid.data);
+            }
+        });
+    });
+
+    return nonNullDataValues;
+};
+
+const WordsBox: React.FC = () => {
+
+    const octagonData: any = useSelector<State>(state => state.entities.octagon);
+
+    return (
+        <Box>
+            <div className={'font-black text-lg mb-5'}>words</div>
+            {getNonNullTrapezoidDataValues(octagonData).map((word, index) => (
+                <div key={index}>{word}</div>
+            ))}
+        </Box>
+    );
+};
+
+export default WordsBox;
