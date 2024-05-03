34564ad510489b0a1b5dcbe96cd15b2ba829a3d6
Joel P <wwwebjoyal@gmail.com>
2024-05-03 23:44:41 +0545

    ColorPicker arrow updates

--- src/components/colorPicker/ColorPicker.tsx
+++ src/components/colorPicker/ColorPicker.tsx
@@ -2,33 +2,40 @@ import React from 'react'
 import HexColors from "@/components/colorPicker/HexColors";
 import CloseIcon from "@/components/common/CloseIcon";
 import ColorPlusIcon from "@/components/colorPicker/ColorPlusIcon";
+import ColorPickerBoxDownArrow from "@/components/colorPicker/ColorPickerBoxDownArrow";
 
 const ColorPicker = ()=>{
     return(
-        <div className={'bg-[#151515] bg-opacity-85 p-4 rounded-[13px] overflow-hidden'}>
+        <div className={'relative'}>
+            <div className={'relative bg-[#151515] bg-opacity-85 p-4 rounded-[13px] overflow-hidden'}>
 
-            <div className={'flex items-center justify-between gap-5 p-1 mb-3'}>
-                <div className={`text-white text-[17px] tracking-tight font-semibold`}>Colors</div>
-                <CloseIcon />
+                <div className={'flex items-center justify-between gap-5 p-1 mb-3'}>
+                    <div className={`text-white text-[17px] tracking-tight font-semibold`}>Colors</div>
+                    <CloseIcon/>
 
-            </div>
-            <HexColors />
+                </div>
+                <HexColors/>
 
-            <div className={'mt-[26px] mb-2 flex gap-8 items-center'}>
-                <div className={`bg-white w-[72px] shrink-0 aspect-square rounded-[10px] overflow-hidden`}></div>
-                <div className={`w-full grid grid-cols-5 gap-3`}>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
-                    <ColorPlusIcon/>
+                <div className={'mt-[26px] mb-2 flex gap-8 items-center'}>
+                    <div className={`bg-white w-[72px] shrink-0 aspect-square rounded-[10px] overflow-hidden`}></div>
+                    <div className={`w-full grid grid-cols-5 gap-3`}>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                        <ColorPlusIcon/>
+                    </div>
+
                 </div>
 
+            </div>
+            <div className={'absolute w-full left-0 bottom-0 translate-y-full flex justify-center'}>
+                <ColorPickerBoxDownArrow/>
             </div>
         </div>
     )
---
+++ src/components/colorPicker/ColorPickerBoxDownArrow.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Icon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="47"
+            height="13"
+            fill="none"
+            viewBox="0 0 47 13"
+        >
+            <g filter="url(#filter0_b_301_12129)">
+                <path
+                    fill="#151515"
+                    fillOpacity="0.85"
+                    fillRule="evenodd"
+                    d="M0 .002c1.712 0 3.424-.009 5.137.01C6.776.03 8.61.055 10.206.657c1.722.65 2.863 1.786 4.01 3.124.827.962 2.45 3.015 3.24 4.006.647.812 1.913 2.418 2.605 3.195.87.978 1.945 2.017 3.44 2.017 1.494 0 2.569-1.04 3.438-2.016.692-.776 1.958-2.383 2.605-3.195.79-.991 2.412-3.044 3.24-4.006 1.149-1.337 2.288-2.474 4.01-3.124 1.596-.6 3.43-.627 5.069-.645 1.712-.019 3.425-.01 5.137-.01"
+                    clipRule="evenodd"
+                ></path>
+            </g>
+            <defs>
+                <filter
+                    id="filter0_b_301_12129"
+                    width="210.097"
+                    height="176.095"
+                    x="-81.549"
+                    y="-81.549"
+                    colorInterpolationFilters="sRGB"
+                    filterUnits="userSpaceOnUse"
+                >
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+                    <feGaussianBlur
+                        in="BackgroundImageFix"
+                        stdDeviation="40.774"
+                    ></feGaussianBlur>
+                    <feComposite
+                        in2="SourceAlpha"
+                        operator="in"
+                        result="effect1_backgroundBlur_301_12129"
+                    ></feComposite>
+                    <feBlend
+                        in="SourceGraphic"
+                        in2="effect1_backgroundBlur_301_12129"
+                        result="shape"
+                    ></feBlend>
+                </filter>
+            </defs>
+        </svg>
+    );
+}
+
+export default Icon;
