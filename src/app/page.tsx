234b199edd95f66cab105291b19bda187f8f3155
Joel.P <wwwebjoyal@gmail.com>
2024-07-03 22:33:41 +0545

    Update Layers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import Properties from "@/components/properties/Properties";
 import Landscape from "@/components/firstLoad/landscape";
 import { useSelector } from "react-redux";
 import Test from "@/components/test/Test";
+import OctagonSVG from "@/components/images/Octagon";
 
 export default function Home() {
   const radius = 80;
@@ -41,7 +42,8 @@ export default function Home() {
         {modules.octagon && (
           <>
             <div className="flex flex-col">
-              <WordsBox />
+              {/* <WordsBox /> */}
+              <Layers />
             </div>
 
             <Octagon
@@ -49,7 +51,7 @@ export default function Home() {
               gap={gap}
               level={distortionActive ? distortionLevel : level}
             />
-            <Properties />
+            {/* <Properties /> */}
           </>
         )}
 
@@ -58,3 +60,32 @@ export default function Home() {
     </div>
   );
 }
+
+const Layers = () => {
+  const activeMenuItem = useSelector(
+    (state: any) => state.entities.header.activeMenuItem
+  );
+
+  const menuItems = {
+    roots: 4,
+    foundations: 3,
+    essence: 2,
+    contributions: 1,
+    constellations: 0,
+  };
+  return (
+    <div className="items-center flex flex-col pt-40 pl-5 pr-10">
+      {Array.from({ length: 5 }, (_, i) => {
+        const isActive = Object.entries(menuItems).some(
+          ([key, value]) => activeMenuItem === key && i === value
+        );
+
+        return (
+          <div key={i} className="relative" style={{ zIndex: 10 - i }}>
+            <OctagonSVG active={isActive} width="200px" marginTop="-150px" />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/components/header/BottomHeader.tsx
+++ src/components/header/BottomHeader.tsx
@@ -40,7 +40,7 @@ const BottomHeader = () => {
         );
       })}
 
-      <Layers />
+    
     </div>
   );
 };
@@ -75,31 +75,3 @@ export const handleClick = (title: string, dispatch: any) => {
   dispatch(activeMenuItemChanged({ item: title }));
 };
 
-const Layers = () => {
-  const activeMenuItem = useSelector(
-    (state: any) => state.entities.header.activeMenuItem
-  );
-
-  const menuItems = {
-    roots: 4,
-    foundations: 3,
-    essence: 2,
-    contributions: 1,
-    constellations: 0,
-  };
-  return (
-    <div className="items-center flex flex-col pt-10 pl-5">
-      {Array.from({ length: 5 }, (_, i) => {
-        const isActive = Object.entries(menuItems).some(
-          ([key, value]) => activeMenuItem === key && i === value
-        );
-
-        return (
-          <div key={i} className="relative" style={{ zIndex: 10 - i }}>
-            <OctagonSVG active={isActive} width="50px" marginTop="-40px" />
-          </div>
-        );
-      })}
-    </div>
-  );
-};
